e17831dcd59d12445430d76fef1190ef30bed547
Emmanuel Campos <ejcampos08@gmail.com>
2022-05-08 19:08:37 +0800

    add progress

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,13 +1,27 @@
-import { ActionIcon, Button, Card, Group, Table, Text, TextInput } from '@mantine/core';
+import { ActionIcon, Card, Group, Table, Text, TextInput } from '@mantine/core';
 import { format, parseISO } from 'date-fns';
 import { GetServerSideProps, NextPage } from 'next';
-import { Coin, Scan, UserCheck } from 'tabler-icons-react';
+import { useEffect, useState } from 'react';
+import { Coin, UserCheck } from 'tabler-icons-react';
 import Nav from '../../components/Nav';
 import QrScanModal from '../../components/QrScanModal';
 
 const Dashboard: NextPage<{ registrations: Registration[] }> = ({ registrations }) => {
-  const rows = registrations.map((r) => (
-    <tr key={r._id}>
+  const [filtered, setFiltered] = useState<Registration[]>([]);
+
+  const search = (value: string) => {
+    const searched = registrations.filter(
+      (r) => r.name.toLowerCase().includes(value.toLowerCase()) || r.course.toLowerCase().includes(value.toLowerCase())
+    );
+    setFiltered(searched);
+  };
+
+  useEffect(() => {
+    setFiltered(registrations);
+  }, []);
+
+  const rows = filtered.map((r, index) => (
+    <tr key={index}>
       <td>
         <Text weight="bold">{r.name}</Text>
         <Text size="xs">
@@ -33,7 +47,7 @@ const Dashboard: NextPage<{ registrations: Registration[] }> = ({ registrations 
       <Nav></Nav>
       <Card mt="md">
         <Group position="apart" mb="lg">
-          <TextInput radius="xl" placeholder="Search" />
+          <TextInput radius="xl" placeholder="Search" onKeyUp={(e) => search(e.currentTarget.value)} />
           <QrScanModal />
         </Group>
         <Table>
